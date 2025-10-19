e32f53fba4417a74abbbe0401276e65b655319cb
dungeonsofpedinburg <dungeonsofpedinburg@gmail.com>
2025-10-19 11:58:18 +0700

    • Добавлен SpeedInsight от Vercel • Добавлена Яндекс Метрика • Переделана дата и место у приключения «Тыквенный спас»

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-// import { SpeedInsights } from "@vercel/speed-insights/next" //
-// import YandexMetrika from "./components/YandexMetrika"; //
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import YandexMetrika from "./components/YandexMetrika";
 
 import localFont from "next/font/local";
 import "./globals.css";
@@ -27,6 +27,8 @@ export default function RootLayout({
         <Header />
         <main className="pt-[7.8rem]">
           {children}
+          <YandexMetrika />
+          <SpeedInsights />
         </main>
         <div id="modal-root"></div>
       </body>
